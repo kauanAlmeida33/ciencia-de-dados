41801c725d2a5459702259ff439034126adc88e1
kauanAlmeida33 <105437759+kauanAlmeida33@users.noreply.github.com>
2024-10-25 08:18:41 -0300

    as

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,20 +1,24 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function visualizarInformacoesGlobais() {
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2);
 
-        const res = await fetch(url)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> bilhões de pessoas e 
+    que aproximadamente <span>${pessoasConectadas}</span> bilhões estão conectadas em alguma rede social e 
+    passam em média <span>${horas} horas</span> e <span>${minutos} minutos
+    </span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span>
+    pessoas conectadas em alguma rede social. `
 
-        const dados = await res.json()
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo);
+}
 
-        console.log(dados);
-
-        const paragrafo = document.createElement('p')
-        paragrafo.classList.add('graicos-container_texto')
-        paragrafo.innerHTML = 'você sabia que o muno tem <span>${dados.total_pessoas_mundo}</span> de pessoas e aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.'
-
-        const container = document.getElementById('graicos-container')
-        container.appendChild(paragrafo)
-
-    }
-
-    visualizarInformacoesGlobais()
+vizualizarInformacoesGlobais();
